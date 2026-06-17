bdcb235448b6c4a2adfa669e849631988a2d8f3d
Mahfoudhi Ghzela <ghzelamahfoudhi@yahoo.fr>
2026-06-17 17:35:32 +0200

    Add Cloudinary and Active Storage configuration

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,7 +1,8 @@
 class Hotel < ApplicationRecord
-   has_many :rooms, dependent: :destroy
+  has_many :rooms, dependent: :destroy
   has_many :reviews, dependent: :destroy
   has_many :bookings, through: :rooms
+  has_one_attached :photo
 
   validates :name, presence: true
   validates :city, presence: true
